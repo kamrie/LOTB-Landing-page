ffa8a05fb003242078d0a10e36575bbda9b29979
kamrie <ifedikeze@gmail.com>
2024-09-02 11:20:58 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+
+
+let typed = new Typed('.typing-effect', {
+    strings: [
+        "hi",
+        "Hello",
+        "Welcome",
+        "We're glad you're here"
+    ],
+    backSpeed:"9",
+    typeSpeed:"9",
+    loop:true
+})
